11ebd4d862149ae70ac092da32642011813e0cab
kbartkko <146842563+kbartkko@users.noreply.github.com>
2024-07-01 16:44:23 +0200

    Update index.js

--- homework-12/js/index.js
+++ homework-12/js/index.js
@@ -138,13 +138,3 @@
 // })
 
 
-
-// function delay(ms) {
-//     return new Promise(resolve => {
-//         setTimeout(resolve, ms)
-//     });
-// };
-
-// delay(3000).then(() => alert('виконалось через 3 секунди'));
-
-
